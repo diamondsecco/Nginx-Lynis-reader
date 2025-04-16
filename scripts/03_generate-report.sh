9d797b856fc0a24d85dec35b9ad8df8bc068fbad
renatoroberto25 <renatoroberto25@yahoo.com.br>
2025-04-16 11:09:14 -0300

    Update 03_generate-report.sh

--- scripts/03_generate-report.sh
+++ scripts/03_generate-report.sh
@@ -5,7 +5,6 @@ AUDITOR="Renato"
 DATE=$(date +%Y-%m-%d)
 TIME=$(date +%H-%M-%S)
 REPORT_DIR="/var/www/lynis-portal/lynis-reports"
-
 FILENAME="lynis-report-${DATE}-${TIME}.txt"
 
 echo "📝 Gerando relatório Lynis..."
@@ -13,9 +12,10 @@ sudo lynis audit system --auditor "$AUDITOR" \
   --report-file "/var/log/lynis-report.dat"
 
 echo "📄 Extraindo dados relevantes..."
-sudo grep -E 'os_version=|kernel_version=|linux_kernel_version=|hostname=|hardening_index=' /var/log/lynis-report.dat > "${REPORT_DIR}/${FILENAME}"
+sudo grep -E 'os_version=|kernel_version=|linux_kernel_version=|hostname=|hardening_index=|firewall_active=|auditd=|ntp_synced=|ssh_permit_root_login=|file_integrity_tool_installed=' /var/log/lynis-report.dat > "${REPORT_DIR}/${FILENAME}"
 
 echo "🔒 Ajustando permissões..."
 sudo chown -R nginx:nginx "$REPORT_DIR"
+sudo chcon -R -t httpd_sys_content_t "$REPORT_DI"
 
 echo "✅ Relatório salvo em: ${REPORT_DIR}/${FILENAME}"
